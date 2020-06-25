9d0def095e6ae873f1af3e3a2e01da1cff0525bd
EDMONDGIHOZO <gihozoedmond@gmail.com>
2020-06-25 18:04:57 +0200

    fix links

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
 export default Axios.create({
-    baseURL: "http://localhost:3331/api/v1",
+    baseURL: "http://backend.aidspan.org/api/v1",
     headers: {
         "Content-type": "application/json"
     }
