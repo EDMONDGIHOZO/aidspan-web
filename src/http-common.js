88e12fb7ba665c7486fbf32f2f64098a1ebcc089
EDMONDGIHOZO <gihozoedmond@gmail.com>
2020-07-03 21:06:18 +0200

    ignoration

--- src/http-common.js
+++
@@ -1,9 +0,0 @@
-import Axios from 'axios'
-
-export default Axios.create({
-    baseURL: "http://backend.aidspan.org/api/v1",
-    //baseURL: "localhost:3331/api/v1",
-    headers: {
-        "Content-type": "application/json"
-    }
-})
