62afc1540ee18adcdae15f37dd06057acd4ad9c0
EDMONDGIHOZO <gihozoedmond@gmail.com>
2020-07-03 18:25:38 +0200

    download

--- src/mixins/downloadIssue.js
+++ src/mixins/downloadIssue.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 export default {
     methods: {
         downloadIssue: function(title) {
@@ -12,10 +13,18 @@ export default {
                 str = str.replace('OFM-Edition ', '')
                 languagedir = 'French'
             }
-            let downloadLink = `${base_url}/sites/default/files/gfo/${str}/${languagedir}/GFO-Issue-${str}.pdf`
-                /// add download power
-            window.location.assign(downloadLink)
-            alert(downloadLink)
+            axios({
+                url: `${base_url}/sites/default/files/gfo/${str}/${languagedir}/GFO-Issue-${str}.pdf`,
+                method: 'GET',
+                responseType: 'blob', // important
+            }).then((response) => {
+                const url = window.URL.createObjectURL(new Blob([response.data]))
+                const link = document.createElement('a')
+                link.href = url
+                link.setAttribute('download', 'file.pdf')
+                document.body.appendChild(link)
+                link.click()
+            })
         },
     },
 }
--- src/views/client/pages/Editorial.vue
+++ src/views/client/pages/Editorial.vue
@@ -1,89 +1,121 @@
 <template>
-<div id="editorial">
+  <div id="editorial">
     <v-container id="gfo-intro">
-        <v-row>
-            <v-col cols="12">
-                <div class="text-center gfotitle">
-                    <h3 class="font-weight-black">{{$t('gfo_intro.title')}}</h3>
-                </div>
-                <div class="content" v-scrollAnimation>
-                    <p class="ma-10">{{$t('gfo_intro.description')}}</p>
-                </div>
-                <div class="bottom-toolbar">
-                    <v-row justify="center">
-                        <v-dialog v-model="dialog" fullscreen hide-overlay transition="slide-x-transition">
-                            <template v-slot:activator="{ on }">
-                                <v-btn color="success" v-if="language === 'fr'" text @click="ofm">TOUS LES NUMÉROS</v-btn>
-                                <v-btn dark v-on="on" text color="primary" v-else>ALL GFO ISSUES</v-btn>
-                                <v-btn text color="secondary" href="#current-issue-editorial">{{$t('currentissue')}}</v-btn>
-                            </template>
-                            <v-card>
-                                <v-toolbar dark color="primary">
-                                    <v-btn icon dark @click="dialog = false">
-                                        <v-icon>mdi-close</v-icon>
-                                    </v-btn>
-                                    <v-toolbar-title>GFO Newsletter</v-toolbar-title>
-                                    <v-spacer></v-spacer>
-                                    <h1>All Issues</h1>
-                                </v-toolbar>
-                                <!--- view all issues component -->
-                                <AllIssues></AllIssues>
-                            </v-card>
-                        </v-dialog>
-                    </v-row>
-                </div>
-            </v-col>
-        </v-row>
+      <v-row>
+        <v-col cols="12">
+          <div class="text-center gfotitle">
+            <h3 class="font-weight-black">{{$t('gfo_intro.title')}}</h3>
+          </div>
+          <div class="content" v-scrollAnimation>
+            <p class="ma-10">{{$t('gfo_intro.description')}}</p>
+          </div>
+          <div class="bottom-toolbar">
+            <v-row justify="center">
+              <v-dialog v-model="dialog" fullscreen hide-overlay transition="slide-x-transition">
+                <template v-slot:activator="{ on }">
+                  <v-btn color="success" v-if="language === 'fr'" text @click="ofm">TOUS LES NUMÉROS</v-btn>
+                  <v-btn dark v-on="on" text color="primary" v-else>ALL GFO ISSUES</v-btn>
+                  <v-btn
+                    text
+                    color="secondary"
+                    href="#current-issue-editorial"
+                  >{{$t('currentissue')}}</v-btn>
+                </template>
+                <v-card>
+                  <v-toolbar dark color="primary">
+                    <v-btn icon dark @click="dialog = false">
+                      <v-icon>mdi-close</v-icon>
+                    </v-btn>
+                    <v-toolbar-title>GFO Newsletter</v-toolbar-title>
+                    <v-spacer></v-spacer>
+                    <h1>All Issues</h1>
+                  </v-toolbar>
+                  <!--- view all issues component -->
+                  <AllIssues></AllIssues>
+                </v-card>
+              </v-dialog>
+            </v-row>
+          </div>
+        </v-col>
+      </v-row>
     </v-container>
     <!--- end of the last live articles -->
     <!-- start the current isssue view -->
     <v-container id="current-issue-editorial">
-        <v-row>
-            <!--- current issue -->
-            <v-col cols="12">
-                <h1 class="text-center text-uppercase" color="primary">{{$t('currentissue')}}</h1>
-                <h3 class="ma-5"> <span class="orange--text">{{currentIssue.title}}</span> | {{currentIssue.changed | formatDate }}</h3>
-                <v-row>
-                    <v-col cols="12" md="6" v-for="article in orderBy(currentIssue.related_articles, 'article_number.field_article_number_value')" :key="article.nid">
-                      <v-card flat class="mb-2" outlined hover router :to="{name: 'article', params: {article_id: article.nid}}" min-height="180">
-                            <v-card-text class="font-weight-bold">
-                                <p class>{{ article.title | str_limit(320) }}</p>
-                            </v-card-text>
-                            <v-card-actions>
-                                <v-chip class="ma-2" color="success" outlined v-for="type in article.article_types" :key="type.id">
-                                    <v-icon left>mdi-note-text-outline</v-icon>
-                                    {{ type.name }}
-                                </v-chip>
-                                <v-spacer></v-spacer>
-                                <v-chip class="ma-2" color="primary" label text-color="white">
-                                    <v-icon left>mdi-label</v-icon>
-                                    Article
-                                    {{ article.article_number.field_article_number_value }}
-                                </v-chip>
-                            </v-card-actions>
-                        </v-card>
-                    </v-col>
-                </v-row>
+      <v-row>
+        <!--- current issue -->
+        <v-col cols="12">
+          <h1 class="text-center text-uppercase" color="primary">{{$t('currentissue')}}</h1>
+          <h3 class="ma-5">
+            <span class="orange--text">{{currentIssue.title}}</span>
+            | {{currentIssue.changed | formatDate }}
+          </h3>
+          <v-row>
+            <v-col
+              cols="12"
+              md="6"
+              v-for="article in orderBy(currentIssue.related_articles, 'article_number.field_article_number_value')"
+              :key="article.nid"
+            >
+              <v-card
+                flat
+                class="mb-2"
+                outlined
+                hover
+                router
+                :to="{name: 'article', params: {article_id: article.nid}}"
+                min-height="180"
+              >
+                <v-card-text class="font-weight-bold">
+                  <p class>{{ article.title | str_limit(320) }}</p>
+                </v-card-text>
+                <v-card-actions>
+                  <v-chip
+                    class="ma-2"
+                    color="success"
+                    outlined
+                    v-for="type in article.article_types"
+                    :key="type.id"
+                  >
+                    <v-icon left>mdi-note-text-outline</v-icon>
+                    {{ type.name }}
+                  </v-chip>
+                  <v-spacer></v-spacer>
+                  <v-chip class="ma-2" color="primary" label text-color="white">
+                    <v-icon left>mdi-label</v-icon>
+                    Article
+                    {{ article.article_number.field_article_number_value }}
+                  </v-chip>
+                </v-card-actions>
+              </v-card>
             </v-col>
-            <!--- end of current issue -->
-            <v-col cols="12">
-                <p class="font-weight-regular white--text text-end">{{currentIssue.changed | formatDate}}</p>
-                <v-card class="pa-5 text-center" color="light-blue lighten-3" flat>
-                    <p>{{$t('gfonote')}}</p>
-                    <v-divider></v-divider>
-                    <p class="py-5">{{$t('contactgfo')}}</p>
-                    <v-card-actions>
-                        <v-btn color="primary" rounded block depressed href="mailto:adele.sulcas@aidspan.org">{{$t('sendemail')}}</v-btn>
-                    </v-card-actions>
-                </v-card>
-            </v-col>
-        </v-row>
+          </v-row>
+        </v-col>
+        <!--- end of current issue -->
+        <v-col cols="12">
+          <p class="font-weight-regular white--text text-end">{{currentIssue.changed | formatDate}}</p>
+          <v-card class="pa-5 text-center" color="light-blue lighten-3" flat>
+            <p>{{$t('gfonote')}}</p>
+            <v-divider></v-divider>
+            <p class="py-5">{{$t('contactgfo')}}</p>
+            <v-card-actions>
+              <v-btn
+                color="primary"
+                rounded
+                block
+                depressed
+                href="mailto:adele.sulcas@aidspan.org"
+              >{{$t('sendemail')}}</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
     </v-container>
     <!--- end of the current issue articles view  -->
     <!---  start the list of all issues  -->
     <v-container id="all-issues"></v-container>
     <features />
-</div>
+  </div>
 </template>
 
 <script>
@@ -123,8 +155,7 @@ export default {
             return this.$store.state.currentIssueArticles.data;
         },
         language() {
-            return localStorage.getItem("lang");
-        }
+            return this.$i18n.locale
     },
     methods: {
         ofm() {
@@ -143,158 +174,158 @@ export default {
 
 <style lang="scss" scoped>
 #gfo-intro {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: #f2fbff;
-    box-shadow: inset 1px 15px 27px rgba(0, 0, 0, 0.1);
-    height: 322px;
-    margin-bottom: 40px;
-    border-radius: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #f2fbff;
+  box-shadow: inset 1px 15px 27px rgba(0, 0, 0, 0.1);
+  height: 322px;
+  margin-bottom: 40px;
+  border-radius: 20px;
 }
 
 #articles-current-container {
-    display: flex;
-    flex-wrap: wrap;
-    flex-direction: row;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
 }
 
 @media only screen and (max-width: 768px) {
-    #gfo-intro {
-        height: auto;
-        position: relative;
-        margin-top: 12%;
-    }
+  #gfo-intro {
+    height: auto;
+    position: relative;
+    margin-top: 12%;
+  }
 
-    #live-articles {
-        height: auto;
-    }
+  #live-articles {
+    height: auto;
+  }
 
-    #gfo-intro .bottom-toolbar {
-        min-width: 100%;
-    }
+  #gfo-intro .bottom-toolbar {
+    min-width: 100%;
+  }
 }
 
 #gfo-intro .title {
-    color: rgba(71, 71, 71, 0.75);
-    text-align: center;
+  color: rgba(71, 71, 71, 0.75);
+  text-align: center;
 }
 
 #gfo-intro .content {
-    line-height: 200%;
-    text-align: center;
-    color: #000000;
-    width: 98%;
+  line-height: 200%;
+  text-align: center;
+  color: #000000;
+  width: 98%;
 }
 
 #gfo-intro .bottom-toolbar {
-    flex-direction: row;
-    flex-grow: 3;
-    align-self: center;
-    background: #ffffff;
-    box-shadow: 0px 1px 23px rgba(0, 0, 0, 0.1);
-    border-radius: 31.5px;
-    margin-bottom: -80px;
-    width: 50%;
-    margin-left: auto;
-    margin-right: auto;
-    height: 60px;
-    text-align: center;
-    padding-top: 11px;
+  flex-direction: row;
+  flex-grow: 3;
+  align-self: center;
+  background: #ffffff;
+  box-shadow: 0px 1px 23px rgba(0, 0, 0, 0.1);
+  border-radius: 31.5px;
+  margin-bottom: -80px;
+  width: 50%;
+  margin-left: auto;
+  margin-right: auto;
+  height: 60px;
+  text-align: center;
+  padding-top: 11px;
 }
 
 #live-articles {
-    height: auto;
-    left: 69px;
-    top: 568px;
-    background: #ffffff;
-    box-shadow: 2px 5px 31px rgba(0, 0, 0, 0.12);
+  height: auto;
+  left: 69px;
+  top: 568px;
+  background: #ffffff;
+  box-shadow: 2px 5px 31px rgba(0, 0, 0, 0.12);
 
-    display: flex;
-    padding: 10px;
+  display: flex;
+  padding: 10px;
 }
 
 #live-articles .head {
-    display: flex;
-    flex-direction: row;
+  display: flex;
+  flex-direction: row;
 }
 
 #live-articles .head h4 {
-    color: #474747;
+  color: #474747;
 }
 
 #live-articles .head h3 {
-    color: #ff7a2b;
+  color: #ff7a2b;
 }
 
 #live-articles .article {
-    background: #ffffff;
-    box-sizing: border-box;
-    border-radius: 14px;
+  background: #ffffff;
+  box-sizing: border-box;
+  border-radius: 14px;
 }
 
 #live-articles .article .gfotitle {
-    font-size: 12px;
-    font-weight: bold;
-    color: #00adef;
+  font-size: 12px;
+  font-weight: bold;
+  color: #00adef;
 }
 
 #current-issue-editorial {
-    height: auto;
-    overflow: hidden;
-    padding: 20px;
-    box-shadow: 2px 5px 31px rgba(0, 0, 0, 0.12);
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
+  height: auto;
+  overflow: hidden;
+  padding: 20px;
+  box-shadow: 2px 5px 31px rgba(0, 0, 0, 0.12);
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 }
 
 .current-issue-articles {
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
-    border-top-right-radius: 20px;
-    border-bottom-right-radius: 20px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
+  border-top-right-radius: 20px;
+  border-bottom-right-radius: 20px;
 }
 
 #current-issue-editorial .new-issue-side {
-    padding: 10px;
-    display: flex;
-    height: 500px;
-    border-radius: 10px;
-    flex-direction: column;
-    align-content: center;
+  padding: 10px;
+  display: flex;
+  height: 500px;
+  border-radius: 10px;
+  flex-direction: column;
+  align-content: center;
 }
 
 #current-issue-editorial .current-cards {
-    border-radius: 5px;
-    font-weight: bold;
-    font-size: 16px;
-    line-height: 19px;
-    min-height: 150px;
-    flex-grow: 2;
-    width: 30%;
+  border-radius: 5px;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 19px;
+  min-height: 150px;
+  flex-grow: 2;
+  width: 30%;
 }
 
 #current-issue-editorial .current-cards:hover {
-    background: #ffffff;
-    box-shadow: 0px 4px 19px rgba(43, 183, 255, 0.46);
-    transform: scale(1.01);
+  background: #ffffff;
+  box-shadow: 0px 4px 19px rgba(43, 183, 255, 0.46);
+  transform: scale(1.01);
 }
 
 #current-issue-editorial .new-issue-side .issue-number {
-    color: white;
+  color: white;
 }
 
 #article-cards {
-    background: rgb(212, 16, 16);
-    box-shadow: 0px 4px 19px rgba(43, 183, 255, 0.46);
-    border-radius: 5px;
-    height: 98px;
+  background: rgb(212, 16, 16);
+  box-shadow: 0px 4px 19px rgba(43, 183, 255, 0.46);
+  border-radius: 5px;
+  height: 98px;
 }
 
 #article-card .title {
-    font-weight: bold;
-    font-size: 12px;
-    line-height: 16px;
-    color: #00adef;
+  font-weight: bold;
+  font-size: 12px;
+  line-height: 16px;
+  color: #00adef;
 }
 </style>
