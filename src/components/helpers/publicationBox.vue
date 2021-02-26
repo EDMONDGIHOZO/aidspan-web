66cf6fde25bf937bd483fe936bab65adf3bc3f72
EDMOND G <gihozoedmond@gmail.com>
2021-02-26 13:46:02 +0200

    publications view
    added the components for the publications view

---
+++ src/components/helpers/publicationBox.vue
@@ -0,0 +1,63 @@
+<template>
+	<div class="container">
+		<div class="box">
+			<v-card rounded flat>
+				<v-card-title>
+					domestic financing for HIV
+				</v-card-title>
+				<div class="subti">
+					<v-card-subtitle>
+						author: kitumaine
+					</v-card-subtitle>
+					<v-card-subtitle>
+						published at 23 dec 2021
+					</v-card-subtitle>
+				</div>
+
+				<v-card-text class="font-weight-bold">
+					<p>
+						{{ description | str_limit }}
+					</p>
+				</v-card-text>
+    <v-divider></v-divider>
+				<v-card-actions>
+					<div class="download-buttons">
+                        <span>files</span>
+						<v-btn color="secondary" icon>
+							<v-icon>mdi-file-pdf</v-icon>
+						</v-btn>
+						<v-btn color="success" icon>
+							<v-icon>mdi-file-word</v-icon>
+						</v-btn>
+					</div>
+				</v-card-actions>
+			</v-card>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			description:
+				"loredsfgsdfgdsfgdsfgdfsgdfgdfgdfgdsfgdfvdfvdsfvdfbdfbfdbsdfbsdfbsdfbdsdf",
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+.subti {
+	display: flex;
+	justify-content: space-between;
+	flex-direction: row;
+}
+
+.download-buttons{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+}
+</style>
--- src/views/client/pages/Publications.vue
+++ src/views/client/pages/Publications.vue
@@ -1,76 +1,97 @@
 <template>
-  <div class="container">
-    <h3>publicationsgma</h3>
-  </div>
+	<div class="wrapper">
+		<div class="titler">
+			<h3>Publications</h3>
+		</div>
+
+		<v-row class="wrapper">
+			<v-col cols="12" md="8">
+				<div class="list-container">
+					<div class="search">
+						<v-text-field
+							label="search for publication"
+							background-color="white"
+							outlined
+							dense
+						></v-text-field>
+					</div>
+
+					<div class="pubs">
+						<pub-box />
+					</div>
+				</div>
+			</v-col>
+			<v-col cols="12" md="4">
+				<v-list dense color="primary" rounded dark>
+					<v-subheader class="tit">CATEGORIES</v-subheader>
+					<v-list-item-group v-model="select_pub" color="primary">
+						<v-list-item v-for="(item, i) in items" :key="i">
+							<v-list-item-content>
+								<v-list-item-title v-text="item.text"></v-list-item-title>
+							</v-list-item-content>
+							<v-list-item-avatar>
+							23
+							</v-list-item-avatar>
+						</v-list-item>
+					</v-list-item-group>
+				</v-list>
+			</v-col>
+		</v-row>
+	</div>
 </template>
 
 <script>
+import PubBox from "@/components/helpers/publicationBox.vue";
 export default {
-  data() {
-    return {
-      headers: [
-        {
-          text: "title",
-          align: "start",
-          value: "title",
-          groupable: false,
-        },
-        { text: "Category", value: "category", align: "right" },
-        { text: "published at", value: "published_at", align: "right" },
-      ],
-      desserts: [
-        {
-          name: "Frozen Yogurt",
-          category: "Ice cream",
-          dairy: "Yes",
-        },
-        {
-          name: "Ice cream sandwich",
-          category: "Ice cream",
-          dairy: "Yes",
-        },
-        {
-          name: "Eclair",
-          category: "Cookie",
-          dairy: "Yes",
-        },
-        {
-          name: "Cupcake",
-          category: "Pastry",
-          dairy: "Yes",
-        },
-        {
-          name: "Gingerbread",
-          category: "Cookie",
-          dairy: "No",
-        },
-        {
-          name: "Jelly bean",
-          category: "Candy",
-          dairy: "No",
-        },
-        {
-          name: "Lollipop",
-          category: "Candy",
-          dairy: "No",
-        },
-        {
-          name: "Honeycomb",
-          category: "Toffee",
-          dairy: "No",
-        },
-        {
-          name: "Donut",
-          category: "Pastry",
-          dairy: "Yes",
-        },
-        {
-          name: "KitKat",
-          category: "Candy",
-          dairy: "Yes",
-        },
-      ],
-    };
-  },
+	name: "publications",
+	data() {
+		return {
+			items: [
+				{ text: "Guides", icon: "mdi-clock" },
+				{ text: "Reports", icon: "mdi-account" },
+			],
+		};
+	},
+	components: {
+		"pub-box": PubBox,
+	},
 };
 </script>
+
+<style lang="scss" scoped>
+.titler h3 {
+	font-family: Poppins;
+	font-style: normal;
+	font-weight: bold;
+	font-size: 40px;
+	line-height: 60px;
+	/* identical to box height */
+	color: #a6acbe;
+}
+
+.list-container {
+	background: rgba(196, 196, 196, 0.12);
+	border: 1px solid #eeeeee;
+	box-sizing: border-box;
+	border-radius: 6px;
+	padding: 13px;
+}
+
+.categories {
+	background: #00adef;
+	border: 1px solid #eeeeee;
+	box-sizing: border-box;
+	border-radius: 6px;
+	padding: 14px;
+}
+
+.search {
+	padding: 13px;
+}
+
+.tit {
+  font-size: 20px;
+	text-transform: uppercase;
+	color: white;
+}
+</style>
