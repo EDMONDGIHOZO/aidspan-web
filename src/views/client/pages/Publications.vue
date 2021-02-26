33d8995599c41ac0ab17dfd746253de205066bb0
EDMONDGIHOZO <gihozoedmond@gmail.com>
2021-02-26 10:52:34 +0200

    make changes to the publication

--- src/views/client/pages/Publications.vue
+++ src/views/client/pages/Publications.vue
@@ -1,63 +1,76 @@
 <template>
-  <div class="publications">
-    <div class="sidenav">
-      <v-btn icon @mouseover="seen = !seen" color="primary">
-        <v-icon>mdi-eye</v-icon>
-      </v-btn>
-      <div class="menus" v-if="!seen">
-        <v-btn depressed rounded color="secondary" class="mx-3" href="#reports">Reports</v-btn>
-        <v-btn depressed rounded color="primary" class="mx-3" href="#guides">Guides</v-btn>
-      </div>
-    </div>
-    <Reports id="reports" />
-    <Guides id="guides" />
+  <div class="container">
+    <h3>publicationsgma</h3>
   </div>
 </template>
+
 <script>
-import Reports from "@/views/client/pages/reports.vue";
-import Guides from "@/views/client/pages/Guides.vue";
 export default {
-  name: "publications",
-  components: {
-    Reports,
-    Guides,
+  data() {
+    return {
+      headers: [
+        {
+          text: "title",
+          align: "start",
+          value: "title",
+          groupable: false,
+        },
+        { text: "Category", value: "category", align: "right" },
+        { text: "published at", value: "published_at", align: "right" },
+      ],
+      desserts: [
+        {
+          name: "Frozen Yogurt",
+          category: "Ice cream",
+          dairy: "Yes",
+        },
+        {
+          name: "Ice cream sandwich",
+          category: "Ice cream",
+          dairy: "Yes",
+        },
+        {
+          name: "Eclair",
+          category: "Cookie",
+          dairy: "Yes",
+        },
+        {
+          name: "Cupcake",
+          category: "Pastry",
+          dairy: "Yes",
+        },
+        {
+          name: "Gingerbread",
+          category: "Cookie",
+          dairy: "No",
+        },
+        {
+          name: "Jelly bean",
+          category: "Candy",
+          dairy: "No",
+        },
+        {
+          name: "Lollipop",
+          category: "Candy",
+          dairy: "No",
+        },
+        {
+          name: "Honeycomb",
+          category: "Toffee",
+          dairy: "No",
+        },
+        {
+          name: "Donut",
+          category: "Pastry",
+          dairy: "Yes",
+        },
+        {
+          name: "KitKat",
+          category: "Candy",
+          dairy: "Yes",
+        },
+      ],
+    };
   },
-  data: () => ({
-    seen: false,
-  }),
 };
 </script>
-<style lang="scss" scoped>
-.sidenav {
-  position: fixed;
-  z-index: 1;
-  overflow-x: hidden;
-  padding: 8px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  right: 0;
-  border-radius: 10px;
-}
-.sidenav .menus {
-  overflow-x: hidden;
-  padding: 8px;
-  border-radius: 4px;
-  box-shadow: 0px 1px 23px rgba(0, 0, 0, 0.1);
-  background-color: white;
-}
-
-
-@media only screen and (max-width: 600px) {
-  .sidenav {
-      bottom: 0;
-  }
-  #reports {
-      margin-top: 40px;
-  }
-}
-
-
-
-
-</style>
