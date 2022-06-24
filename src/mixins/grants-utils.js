fd8add6846914ad68475266d51a685350d02c460
edmond gihozo <34583961+EDMONDGIHOZO@users.noreply.github.com>
2022-06-24 18:05:00 +0200

    data presentation for regions

--- src/mixins/grants-utils.js
+++ src/mixins/grants-utils.js
@@ -1,19 +1,43 @@
 import _ from "lodash";
 
 export default {
-    methods: {
-        formatNumbers: function (number) {
-            return number.toLocaleString('en-US')
-        },
-        sumValues: function (data, sumKey) {
-            if (data && data.length) {
-                const summed = _.sumBy(data, sumKey)
-                return this.formatNumbers(summed)
-            }
-        },
-        percentageFinder: function (partialValue, totalValue) {
-           const percentage = 100 * parseInt(partialValue) / parseInt(totalValue)
-            return (percentage.toFixed(0));
+  methods: {
+    formatNumbers: function(number) {
+      return number.toLocaleString("en-US");
+    },
+    sumValues: function(data, sumKey) {
+      if (data && data.length) {
+        const summed = _.sumBy(data, sumKey);
+        return this.formatNumbers(summed);
+      }
+    },
+    percentageFinder: function(partialValue, totalValue) {
+      const percentage = (100 * parseInt(partialValue)) / parseInt(totalValue);
+      return percentage.toFixed(0);
+    },
+    mergeArrays: function(firstArray, secondArray, firstKey, secondKey) {
+      const results = firstArray.map((firstArrayObject) => ({
+        ...secondArray.find(
+          (secondArrayObject) =>
+            firstArrayObject[firstKey] === secondArrayObject[secondKey] &&
+            secondArrayObject
+        ),
+        ...firstArrayObject,
+      }));
+      return results;
+    },
+
+    groupObjects: function(data, groupingkey) {
+      const groupedData = data.reduce((acc, value) => {
+        if (!acc[value[groupingkey]]) {
+          acc[value[groupingkey]] = [];
         }
-    }
-}
+        // Grouping
+        acc[value[groupingkey]].push(value);
+        return acc;
+      }, {});
+
+      return Object.entries(groupedData);
+    },
+  },
+};
--- src/views/client/pages/grants-portfolio.vue
+++ src/views/client/pages/grants-portfolio.vue
@@ -1,91 +1,159 @@
 <template>
   <v-container>
     <div class="intro-title my-4">
-      <h3>
-        Grants Portfolio
-      </h3>
+      <h3>Grants Portfolio</h3>
     </div>
+    <section>
+      <div class="intro-box">
+        <p>
+          The information provided here is based entirely on data obtained from
+          the Global Fund, most of which is on the Fund's website. The Aidspan
+          web server checks every three hours for possible new data at the
+          Fund's website. Every few months, the Global Fund assigns each grant a
+          rating that measures the performance of that grant over the past few
+          months. These ratings are (“Exceeds expectations”), A2 (“Meets
+          expectations”), B1 (“Adequate”), B2 (“Inadequate but potential
+          demonstrated”) or C (“Unacceptable”). In the map below, we show the
+          average rating assigned by the Global Fund since January 2010 for
+          grants to each country. Where a country has had fewer than four
+          ratings assigned, no average rating is shown.
+        </p>
+      </div>
+    </section>
     <v-row id="grants-tables" width="100%">
       <v-col cols="12">
-        <div class="intro-box">
-          <p>
-            The information provided here is based entirely on data obtained from the Global Fund, most of which is on
-            the Fund's website. The Aidspan web server checks every three hours for possible new data at the Fund's
-            website. Every few months, the Global Fund assigns each grant a rating that measures the performance of that
-            grant over the past few months. These ratings are (“Exceeds expectations”), A2 (“Meets expectations”), B1
-            (“Adequate”), B2 (“Inadequate but potential demonstrated”) or C (“Unacceptable”). In the map below, we show
-            the average rating assigned by the Global Fund since January 2010 for grants to each country. Where a
-            country has had fewer than four ratings assigned, no average rating is shown.
-          </p>
+        <div v-if="allLoaded">
+          <div class="grants-summary">
+            <div>
+              <h3>Total Agreement Amount:</h3>
+              <h1>{{ sumValues(allGrants, "totalSignedAmount") }} USD</h1>
+            </div>
+            <div>
+              <h3>Total Disbursed Amount:</h3>
+              <h1>{{ sumValues(allGrants, "totalDisbursedAmount") }} USD</h1>
+            </div>
+          </div>
+          <v-simple-table fixed-header id="disease-table" dark>
+            <template v-slot:default>
+              <thead>
+                <tr>
+                  <th colspan="3" class="text-center">
+                    <h4 class="text-uppercase text--white">
+                      Grants by Regions
+                    </h4>
+                  </th>
+                </tr>
+                <tr>
+                  <th class="text-left">Region</th>
+                  <th class="text-center">Total agreement amount</th>
+                  <th class="text-right">Total disbursed to date</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(region, index) in grantsByRegion" :key="index">
+                  <td class="text-left">{{ region[0] }}</td>
+                  <td>
+                    <div class="row-box">
+                      <p>
+                        $ {{ sumValues(region[1], "totalCommittedAmount") }}
+                      </p>
+                      <v-chip
+                        class="percentage-chip align-left"
+                        color="success"
+                        outlined
+                      >
+                        {{
+                          percentageFinder(
+                            sumValues(region[1], "totalCommittedAmount"),
+                            sumValues(region[1], "totalSignedAmount")
+                          )
+                        }}
+                        <v-icon right small>mdi-percent</v-icon>
+                      </v-chip>
+                    </div>
+                  </td>
+                  <td class="text-right">
+                    <div class="row-box">
+                      <p>
+                        $ {{ sumValues(region[1], "totalDisbursedAmount") }}
+                      </p>
+                      <v-chip class="percentage-chip" color="primary" outlined>
+                        {{
+                          percentageFinder(
+                            sumValues(region[1], "totalDisbursedAmount"),
+                            sumValues(region[1], "totalSignedAmount")
+                          )
+                        }}
+                        <v-icon right small>mdi-percent</v-icon>
+                      </v-chip>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
+          <v-simple-table fixed-header id="disease-table" dark class="mt-6 rounded">
+            <template v-slot:default>
+              <thead>
+                <tr>
+                  <th colspan="3" class="text-center">
+                    <h4 class="text-uppercase text--white">
+                      Grants by Diseases
+                    </h4>
+                  </th>
+                </tr>
+                <tr>
+                  <th class="text-left">Disease</th>
+                  <th class="text-center">Total agreement amount</th>
+                  <th class="text-right">Total disbursed to date</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(disease, index) in diseaseComponents" :key="index">
+                  <td class="text-left">{{ disease[0] }}</td>
+                  <td>
+                    <div class="row-box">
+                      <p>
+                        $ {{ sumValues(disease[1], "totalCommittedAmount") }}
+                      </p>
+                      <v-chip
+                        class="percentage-chip align-left"
+                        color="success"
+                        outlined
+                      >
+                        {{
+                          percentageFinder(
+                            sumValues(disease[1], "totalCommittedAmount"),
+                            sumValues(disease[1], "totalSignedAmount")
+                          )
+                        }}
+                        <v-icon right small>mdi-percent</v-icon>
+                      </v-chip>
+                    </div>
+                  </td>
+                  <td class="text-right">
+                    <div class="row-box">
+                      <p>
+                        $ {{ sumValues(disease[1], "totalDisbursedAmount") }}
+                      </p>
+                      <v-chip class="percentage-chip" color="primary" outlined>
+                        {{
+                          percentageFinder(
+                            sumValues(disease[1], "totalDisbursedAmount"),
+                            sumValues(disease[1], "totalSignedAmount")
+                          )
+                        }}
+                        <v-icon right small>mdi-percent</v-icon>
+                      </v-chip>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
         </div>
-      </v-col>
-      <v-col cols="12" md="8">
-        <v-simple-table
-            fixed-header
-            id="disease-table"
-            v-if="diseaseLoaded"
-        >
-          <template v-slot:default>
-            <thead>
-            <tr>
-              <th colspan="3" class="text-center">
-                <h4 class="text-uppercase text--white">
-                  Grants by Diseases
-                </h4>
-              </th>
-            </tr>
-            <tr>
-              <th class="text-left">Disease</th>
-              <th class="text-center">Total agreement amount</th>
-              <th class="text-right">Total disbursed to date</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="(disease, index) in diseaseComponents" :key="index">
-              <td class="text-left">{{ disease[0] }}</td>
-              <td>
-                <div class="row-box">
-                  <p>$ {{ sumValues(disease[1], "totalCommittedAmount") }}</p>
-                  <v-chip class="percentage-chip align-left" color="success" outlined>
-                    {{
-                      percentageFinder(sumValues(disease[1], "totalCommittedAmount"), sumValues(disease[1], "totalSignedAmount"))
-                    }}
-                    <v-icon right small>mdi-percent</v-icon>
-                  </v-chip>
-                </div>
-              </td>
-              <td class="text-right">
-                <div class="row-box">
-                  <p>$ {{ sumValues(disease[1], "totalDisbursedAmount") }}</p>
-                  <v-chip class="percentage-chip" color="primary" outlined>
-                    {{
-                      percentageFinder(sumValues(disease[1], "totalDisbursedAmount"), sumValues(disease[1], "totalSignedAmount"))
-                    }}
-                    <v-icon right small>mdi-percent</v-icon>
-                  </v-chip>
-                </div>
-              </td>
-            </tr>
-            <tr class="diseaseFooter">
-              <td colspan="2">
-                <small>Total Agreement Amount:</small>
-                <br/>
-                {{ sumValues(allGrants, 'totalSignedAmount') }}
-              </td>
-              <td colspan="2">
-                <small>Total Disbursed Amount:</small>
-                <br/>
-                {{ sumValues(allGrants, 'totalDisbursedAmount') }}
-              </td>
-            </tr>
-            </tbody>
-          </template>
-        </v-simple-table>
         <div v-else class="shadow">
-          <v-skeleton-loader
-              class="mx-auto"
-              type="table"
-          />
+          <v-skeleton-loader class="mx-auto" type="table" />
         </div>
       </v-col>
     </v-row>
@@ -93,7 +161,7 @@
     <!-- start the grants by country map presentation using D3 -->
     <v-row class="py-5">
       <v-col cols="12">
-        <countriesMap/>
+        <countriesMap />
       </v-col>
     </v-row>
     <!-- end of the map -->
@@ -103,7 +171,10 @@
 <script>
 import GrantsCountry from "@/components/pages/client/grants-country.vue";
 import sumValues from "@/mixins/grants-utils";
+import mergeArrays from "@/mixins/grants-utils";
+import groupObjects from "@/mixins/grants-utils";
 import GFAPI from "@/services/GlobalFundApi";
+import worldData from "../../../assets/dummies/world.json";
 
 export default {
   //dummy data
@@ -112,13 +183,12 @@ export default {
       totalDisbursementAmount: 0,
       totalAgreementAmount: 0,
       diseaseComponents: [],
-      diseaseLoaded: false,
+      grantsByRegion: [],
+      allLoaded: false,
       allGrants: [],
       //data for charts options//
-
       ///end of chart
       diseases: [],
-      regions: [],
     };
   },
 
@@ -128,31 +198,34 @@ export default {
 
   methods: {
     async fetchAllGrantsData() {
-      const results = await GFAPI().get('/VGrantAgreements');
-      if (results.statusText === 'OK') {
-        const grants = results.data.value
-        this.allGrants = grants;
+      const results = await GFAPI().get("/VGrantAgreements");
+      if (results.statusText === "OK") {
+        const grants = results.data.value;
         // eslint-disable-next-line no-undef
-        const groupedByDisease = grants.reduce((acc, value) => {
-          // Group initialization
-          if (!acc[value.componentName]) {
-            acc[value.componentName] = [];
-          }
-          // Grouping
-          acc[value.componentName].push(value);
-          return acc;
-        }, {});
-        this.diseaseComponents = Object.entries(groupedByDisease);
-        this.diseaseLoaded = true
+        this.diseaseComponents = this.groupObjects(grants, "componentName");
+        // merge data
+        const onlyCountries = grants.filter(
+          (c) => c.geographicAreaCode_ISO2 !== null
+        );
+        const merged = await this.mergeArrays(
+          onlyCountries,
+          worldData,
+          "geographicAreaCode_ISO3",
+          "iso_alpha3_code"
+        );
+        // group by regions then
+        this.grantsByRegion = this.groupObjects(merged, "Sub_region_name");
+        this.allGrants = merged;
+        this.allLoaded = true;
         // eslint-disable-next-line no-undef
       }
-    }
+    },
   },
 
   mounted: function () {
     this.fetchAllGrantsData();
   },
 
-  mixins: [sumValues]
+  mixins: [sumValues, mergeArrays, groupObjects],
 };
 </script>
